14ded5cb84f12c182f25f93eb5d76d2fb128cff1
skygrimm <skygrimm.super@gmail.com>
2025-03-19 12:48:18 +0200

    adding some syles

---
+++ app/src/main/java/com/example/composition/presentation/GameMainMenuFragment.kt
@@ -0,0 +1,23 @@
+package com.example.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.composition.R
+import com.example.composition.databinding.FragmentGameMainMenuBinding
+
+class GameMainMenuFragment : Fragment() {
+    private lateinit var binding: FragmentGameMainMenuBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentGameMainMenuBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
